ea2652ff3249bb0a2164251f5a81e5507113aba6
chayobi03-cyber <chayobi03@gmail.com>
2026-09-09 20:46:33 +0900

    refactor: expose daily historical ingest API

--- src/investment_pipeline/history/__init__.py
+++ src/investment_pipeline/history/__init__.py
@@ -1,6 +1,14 @@
 """Historical investment state and decision persistence."""
 
+from .daily import ingest_daily_bundle
 from .models import DecisionLog, MarketSnapshot, OutcomeLog, StockSnapshot
 from .store import HistoricalStore
 
-__all__ = ["DecisionLog", "HistoricalStore", "MarketSnapshot", "OutcomeLog", "StockSnapshot"]
+__all__ = [
+    "DecisionLog",
+    "HistoricalStore",
+    "MarketSnapshot",
+    "OutcomeLog",
+    "StockSnapshot",
+    "ingest_daily_bundle",
+]
